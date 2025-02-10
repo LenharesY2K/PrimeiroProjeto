339cc1a3ea128118196a5f7cd3f3418b9b27bf12
LenharesY2K <lenharespedro@gmail.com>
2025-02-10 18:11:40 -0300

    primary commit from pwiii

---
+++ PrimeiroProjeto/usuarios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto
+{
+    internal class usuarios
+    {
+        public int id { get; set;}
+        public string login { get ; set;}
+
+        public string senha  { get; set;}
+
+    }
+}
